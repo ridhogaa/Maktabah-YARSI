005b460b975c77ea046f9aacc0de80f95daba36e
Ridho Gymnastiar Al Rasyid <69922597+ridhogaa@users.noreply.github.com>
2023-09-18 14:58:19 +0700

    add:dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()  // Maven Central repository
+    }
+    dependencies {
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.2")
+        classpath("com.android.tools.build:gradle:3.4.3")
+    }
+}
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    id("com.android.library") version "7.4.1" apply false
+    id("com.google.dagger.hilt.android") version "2.44" apply false
 }
