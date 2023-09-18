7b4065a1c08dd54d5deb2b46fccdacac4a66a0fc
Ridho Gymnastiar Al Rasyid <69922597+ridhogaa@users.noreply.github.com>
2023-09-18 20:07:09 +0700

    add:dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -57,8 +57,8 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.1")
 
     // Navigation Fragment & UI
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
 
     // Retrofit & Okhttp
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
