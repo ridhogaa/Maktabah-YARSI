a47935d0982bc548abef0b5d586264d8d83aa901
dianprnms <purnamasaridian010@gmail.com>
2023-09-21 20:47:41 +0700

    Image Slider

--- app/src/main/java/com/maktabah/maktabahyarsi/ui/home/HomeFragment.kt
+++ app/src/main/java/com/maktabah/maktabahyarsi/ui/home/HomeFragment.kt
@@ -7,35 +7,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.viewpager.widget.ViewPager
 import com.maktabah.maktabahyarsi.R
 import com.maktabah.maktabahyarsi.databinding.FragmentHomeBinding
-import com.maktabah.maktabahyarsi.databinding.FragmentSplashScreenBinding
-import com.maktabah.maktabahyarsi.ui.home.adapter.CategoryAdapter
 import com.maktabah.maktabahyarsi.ui.home.slider.SliderAdapter
-import com.maktabah.maktabahyarsi.ui.splashscreen.SplashScreenViewModel
-import com.maktabah.maktabahyarsi.utils.DataCategory
 
 class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
-    private val binding get() = _binding!!
-    private val viewModel: HomeViewModel by viewModels()
-    lateinit var vpSlider: ViewPager
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
-        return binding.root
+    companion object {
+        fun newInstance() = HomeFragment()
     }
 
+    private lateinit var viewModel: HomeViewModel
+    private lateinit var binding: FragmentHomeBinding
+    lateinit var vpSlider: ViewPager
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        vpSlider = view.findViewById(R.id.viewPager)
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+
+        vpSlider = binding.viewPager
 
         val arraySlider = ArrayList<Int>()
         arraySlider.add(R.drawable.img_example)
@@ -43,16 +39,16 @@ class HomeFragment : Fragment() {
 
         val sliderAdapter = SliderAdapter(arraySlider, requireActivity())
         vpSlider.adapter = sliderAdapter
-        binding.rvCategory.apply {
-            adapter = CategoryAdapter(DataCategory.listDataCategory)
-            layoutManager = GridLayoutManager(requireContext(), 4)
-            setHasFixedSize(true)
-        }
+
+        return binding.root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        // TODO: Use the ViewModel
+
     }
 
 }
