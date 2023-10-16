ed686893071f3fd15f401407041bf983a1b2bb44
Ridho Gymnastiar Al Rasyid <69922597+ridhogaa@users.noreply.github.com>
2023-10-17 01:40:12 +0700

    feat:splash and feat:onboarding

--- app/src/main/java/com/maktabah/maktabahyarsi/ui/MainActivity.kt
+++ app/src/main/java/com/maktabah/maktabahyarsi/ui/MainActivity.kt
@@ -1,32 +1,37 @@
 package com.maktabah.maktabahyarsi.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
+import android.view.ViewTreeObserver
+import android.view.animation.AnticipateInterpolator
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.maktabah.maktabahyarsi.R
 import com.maktabah.maktabahyarsi.databinding.ActivityMainBinding
-import com.maktabah.maktabahyarsi.databinding.FragmentSplashScreenBinding
-import com.maktabah.maktabahyarsi.ui.home.HomeFragment
-import com.maktabah.maktabahyarsi.ui.search.SearchFragment
-import com.maktabah.maktabahyarsi.ui.setting.SettingFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private var isReady = false
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
     private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setupPreDrawListener()
+        }
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment = binding.fragmentContainerView.getFragment<Fragment>() as NavHostFragment
+        val navHostFragment =
+            binding.fragmentContainerView.getFragment<Fragment>() as NavHostFragment
 
         navController = navHostFragment.navController
         binding.bottomNav.setupWithNavController(navController)
@@ -40,6 +45,28 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun setupPreDrawListener() {
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    // Check if the initial data is ready.
+                    return if (isReady) {
+                        // The content is ready; start drawing.
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+                        true
+                    } else {
+                        Handler().postDelayed(Runnable {
+                            isReady = true
+                        }, 100)
+                        // The content is not ready; suspend.
+                        false
+                    }
+                }
+            }
+        )
+    }
+
     private fun hideBottomNav(hide: Boolean) {
         if (hide) {
             binding.bottomNav.visibility = View.GONE
@@ -50,6 +77,26 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    private fun installSplashScreenWithAnim() {
+        installSplashScreen().setOnExitAnimationListener { splashScreenView ->
+            // Create custom animation.
+            splashScreenView.iconView.animate().rotation(180F).duration = 500L
+            val slideUp = ObjectAnimator.ofFloat(
+                splashScreenView.iconView,
+                View.TRANSLATION_Y,
+                0f,
+                -splashScreenView.iconView.height.toFloat()
+            )
+
+            slideUp.apply {
+                interpolator = AnticipateInterpolator()
+                duration = 500L
+                doOnEnd { splashScreenView.remove() }
+                start()
+            }
+        }
     }
 
     override fun onDestroy() {
